afd03773e14609d500452313beec1576b8a15f54
VladKhrlmv <kharlamov-vlad03@mai.ru>
2021-12-29 17:23:50 +0100

    Add get and post cells/file

---
+++ packages/cli/dist/notebook.js
@@ -0,0 +1,1 @@
+[{"content":"This is a test text cell 2","type":"text","id":"vb6"},{"content":"This is the third rext jaslkdfjsaldkfjsalkdfjslakdfjlaskdfj","type":"text","id":"8nt"},{"content":"","type":"code","id":"mjy"},{"content":"This is a test text cell","type":"text","id":"vb7"}]
